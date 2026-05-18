df7d58e4f34073133fb00594679066b009d9d7af
Ashish <asku3000@gmail.com>
2026-05-18 18:36:24 +0530

    changes

--- cafe-ctrl-ui/src/services/api.js
+++ cafe-ctrl-ui/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: process.env.REACT_APP_BACKEND_URL ? `${process.env.REACT_APP_BACKEND_URL}/api` : 'http://localhost:8080/api',
+  withCredentials: true,
 });
 
 // This is like an Angular Interceptor. 
